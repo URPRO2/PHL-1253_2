58a42eccd2d1a4ab987ee5c4d5ed561daaf533da
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-07-24 21:36:31 +0000

    corrected comment

--- Code/base/wechat.py
+++ Code/base/wechat.py
@@ -1,1 +1,13 @@
-#!/usr
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import requests
+import json
+import os
+
+
+class WeChat:
+    def __init__(self):
+        self.CORPID = 'ww3ffaaf40c09cf232'  #企业ID，在管理后台获取
+  
