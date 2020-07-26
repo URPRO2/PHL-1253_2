4a87534c4855dd2179364be29c4a1b9f51214b79
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-07-26 19:13:41 +0000

    updating translations

--- Code/base/wechat.py
+++ Code/base/wechat.py
@@ -12,4 +12,10 @@ class WeChat:
         self.CORPID = 'ww3ffaaf40c09cf232'  #企业ID，在管理后台获取
         self.CORPSECRET = 'IZ1XYgpPlldPAnGf3j-q1lx5tFTuLXz9kSt2DVkRRFM'#自建应用的Secret，每个自建应用里都有单独的secret
         self.AGENTID = '1000004'  #应用ID，在后台应用中获取
-        self.TOUSER = "ZhouJian"  # 接收者用
+        self.TOUSER = "ZhouJian"  # 接收者用户名,多个用户用|分割
+
+    def _get_access_token(self):
+        url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken'
+        values = {'corpid': self.CORPID,
+                  'corpsecret': self.CORPSECRET,
+        
