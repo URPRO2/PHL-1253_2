f951cdd833633c60e9e715d7c228d30b931ec195
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-08-10 14:20:20 +0000

    local update

--- Code/daxiaopan.py
+++ Code/daxiaopan.py
@@ -6,4 +6,16 @@ import sys,os
 if sys.platform != 'win32':
     sys.path.append('/root/coin2021')
 from base.Tool import *
-from Code.base import w
+from Code.base import wechat
+
+# print(ccxt.__version__)  # 检查ccxt版本，需要最新版本，1.44.21以上
+wx = wechat.WeChat()
+rule_type = '4h'
+num = 20
+bigSymbol = 'BTC/USDT'
+smallSymbol = 'ETH/USDT'
+
+ex = ccxt.binance()
+
+
+def getMom(symbo
