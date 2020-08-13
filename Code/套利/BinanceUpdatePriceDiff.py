b9b6e45a0fc2c8df3da5531807d7551dea8be8dd
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-08-13 12:42:13 +0000

    Remove commented code.

--- Code/套利/BinanceUpdatePriceDiff.py
+++ Code/套利/BinanceUpdatePriceDiff.py
@@ -7,4 +7,10 @@ import datetime
 import ccxt
 
 time_interval = 5  # 数据抓取间隔时间
-diff_target = 0.08  
+diff_target = 0.08  # 目标开仓期现价差 0.08代表8%价差
+exchange = ccxt.binance()
+symbols = exchange.dapiPublicGetExchangeInfo()['symbols']  # 币本位所有交易对
+
+quarterly_symbols_ID = []  # 初始化当季交易对
+for symbol in symbols:
+    if '06' in symbol['symb
