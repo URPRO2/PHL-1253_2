93a0c8870fce0c5e90a036b610f8c4df4d41c93c
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-09-12 20:42:09 +0000

    Update version to 0.1.1

--- program/1_初窥门径/数字货币量化投资概览/5_一个量化策略案例：定投（python）.py
+++ program/1_初窥门径/数字货币量化投资概览/5_一个量化策略案例：定投（python）.py
@@ -2,4 +2,14 @@
 《邢不行-2020新版|Python数字货币量化投资课程》
 无需编程基础，助教答疑服务，专属策略网站，一旦加入，永续更新。
 课程详细介绍：https://quantclass.cn/crypto/class
-邢
+邢不行微信: xbx9025
+本程序作者: 邢不行/西蒙斯
+
+介绍数字货币定投的简单策略
+"""
+import pandas as pd  # 第三方库，专门用于数据分析，处理表格数据
+pd.set_option('expand_frame_repr', False)  # 照抄即可，不求甚解
+
+
+# ===读取数据
+df = pd.read_csv('EOSUSD_1D.csv',  # 此处为
