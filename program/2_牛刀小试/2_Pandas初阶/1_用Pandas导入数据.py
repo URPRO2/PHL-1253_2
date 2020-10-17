0cb9bcde6e87e5fddf93baeff9eb4552857c5e58
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-10-17 14:19:25 +0000

    properties van worksheet invullen via formulier

--- program/2_牛刀小试/2_Pandas初阶/1_用Pandas导入数据.py
+++ program/2_牛刀小试/2_Pandas初阶/1_用Pandas导入数据.py
@@ -10,4 +10,11 @@
 - 如何使用pandas导入数据
 - DataFrame数据结构介绍
 """
-import pandas as pd  # 将pandas作为第三方库导入，
+import pandas as pd  # 将pandas作为第三方库导入，我们一般为pandas取一个别名叫做pd
+
+# =====导入数据
+df = pd.read_csv(
+    # 该参数为数据在电脑中的路径，
+    # 要注意字符串转义符号 \ ，可以使用加r变为raw string或者每一个进行\\转义
+    filepath_or_buffer=r'../../../data/OKEX_20200302_5T.csv',
+    # 编码格式，不同的文件有不同的编码方式，一般文件中
