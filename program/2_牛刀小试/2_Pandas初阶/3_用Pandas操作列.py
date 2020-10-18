0675a2d5af705528e42abbeee0cab395b8eb3b3c
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-10-18 19:31:52 +0000

    Release 3.9.0

--- program/2_牛刀小试/2_Pandas初阶/3_用Pandas操作列.py
+++ program/2_牛刀小试/2_Pandas初阶/3_用Pandas操作列.py
@@ -1,4 +1,14 @@
 """
 《邢不行-2020新版|Python数字货币量化投资课程》
 无需编程基础，助教答疑服务，专属策略网站，一旦加入，永续更新。
-课程详细介绍：https://quantclass.cn/cr
+课程详细介绍：https://quantclass.cn/crypto/class
+邢不行微信: xbx9025
+本程序作者: 邢不行/西蒙斯
+
+# 课程内容
+- 列操作
+- 针对列的统计函数
+"""
+import pandas as pd  # 将pandas作为第三方库导入，我们一般为pandas取一个别名叫做pd
+
+pd.set_option('expand_frame_repr', False)  # 当列太多
