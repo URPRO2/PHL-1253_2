9558c6358629a0080c3b4d271ae2e852aaf11069
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-11-08 22:12:41 +0000

    Update Ring For Android

--- program/2_牛刀小试/2_Pandas初阶/5_用Pandas处理空缺值.py
+++ program/2_牛刀小试/2_Pandas初阶/5_用Pandas处理空缺值.py
@@ -1,4 +1,15 @@
 """
 《邢不行-2020新版|Python数字货币量化投资课程》
 无需编程基础，助教答疑服务，专属策略网站，一旦加入，永续更新。
-课程详细介
+课程详细介绍：https://quantclass.cn/crypto/class
+邢不行微信: xbx9025
+本程序作者: 邢不行/西蒙斯
+
+# 课程内容
+- 删除缺失值
+- 补全缺失值
+- 找出缺失值
+"""
+import pandas as pd  # 将pandas作为第三方库导入，我们一般为pandas取一个别名叫做pd
+
+pd.set_option('expand_frame_repr', False)  # 当列太多时
