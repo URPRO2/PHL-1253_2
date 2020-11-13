a9549edeb145750e1e6800f31d14dcea50111439
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-11-13 13:06:50 +0000

    PMD fixes

--- program/2_牛刀小试/2_Pandas初阶/5_用Pandas处理空缺值.py
+++ program/2_牛刀小试/2_Pandas初阶/5_用Pandas处理空缺值.py
@@ -12,4 +12,15 @@
 """
 import pandas as pd  # 将pandas作为第三方库导入，我们一般为pandas取一个别名叫做pd
 
-pd.set_option('expand_frame_repr', False)  # 当列太多时
+pd.set_option('expand_frame_repr', False)  # 当列太多时清楚展示
+
+# =====导入数据
+df = pd.read_csv(
+    filepath_or_buffer=r'C:\Users\Simons\Desktop\xbx_coin_2020\data\OKEX_BTC-USDT_20200302_1T.csv',
+    encoding='gbk',
+    skiprows=1
+)
+
+# ===== 缺失值处理：原始数据中存在缺失值，如何处理？
+# 0.创建缺失值
+#
