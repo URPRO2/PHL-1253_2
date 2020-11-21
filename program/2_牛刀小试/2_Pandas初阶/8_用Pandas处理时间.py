bb7a3b52720d8936dd248d1ba34417990a25e41c
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-11-21 22:35:13 +0000

    target: Fix incorrect usage of nested IRQ spinlocks in ABORT_TASK path

--- program/2_牛刀小试/2_Pandas初阶/8_用Pandas处理时间.py
+++ program/2_牛刀小试/2_Pandas初阶/8_用Pandas处理时间.py
@@ -10,4 +10,14 @@
 """
 import pandas as pd  # 将pandas作为第三方库导入，我们一般为pandas取一个别名叫做pd
 
-pd.set_option
+pd.set_option('expand_frame_repr', False)  # 当列太多时清楚展示
+
+# =====导入数据
+df = pd.read_csv(
+    r'C:\Users\Simons\Desktop\xbx_coin_2020\data\OKEX_BTC-USDT_20200302_1T.csv',
+    encoding='gbk',
+    skiprows=1
+)
+
+# ===== 时间处理
+# print(df[
