6d0f6a5e2bc3e1ccbeff8d19f45673957daa235f
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-11-24 08:59:54 +0000

    GUI Update

--- program/2_牛刀小试/2_Pandas初阶/9_Pandas的滚动操作.py
+++ program/2_牛刀小试/2_Pandas初阶/9_Pandas的滚动操作.py
@@ -9,4 +9,11 @@
 - Rolling操作
 - Expanding操作
 - 输出到本地文件
-""
+"""
+import pandas as pd  # 将pandas作为第三方库导入，我们一般为pandas取一个别名叫做pd
+
+pd.set_option('expand_frame_repr', False)  # 当列太多时清楚展示
+
+# =====导入数据
+df = pd.read_csv(
+    r'C:\Users\Simons\Desktop\xbx_coin_2020\data\OKEX_B
