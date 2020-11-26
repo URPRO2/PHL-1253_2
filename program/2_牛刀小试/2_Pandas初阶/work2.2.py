64d1ab0bea824d74ac4b4648d38bc297884932bc
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-11-26 14:34:14 +0000

    Woops!

--- program/2_牛刀小试/2_Pandas初阶/work2.2.py
+++ program/2_牛刀小试/2_Pandas初阶/work2.2.py
@@ -7,4 +7,12 @@ from dateutil.relativedelta import relativedelta
 
 # df['candle_begin_time_GMT8'] = pd.to_datetime(df['candle_begin_time'], unit='ms') + timedelta(hours=8)  # 北京时间
 # df.drop(columns=['candle_begin_time'], inplace=True)
-# df = df[['candle_begin_time_GMT8', 'open', 'high', 'low', 'close'
+# df = df[['candle_begin_time_GMT8', 'open', 'high', 'low', 'close', 'volume']]
+
+# print(df)
+
+
+def updateSymbolData(ex, symbol, timeframe, startTime, endTime):
+    dfList = []
+    while startTime < endTime:
+        data = ex.fetch_ohlcv(symbol, timeframe=timeframe, limit=2000, since=ex.pars
