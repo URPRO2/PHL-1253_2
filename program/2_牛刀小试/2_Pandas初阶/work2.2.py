400f32926ca57ab5e8448df0d1caeceeeb45e9d2
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-11-26 06:41:09 +0000

    merge into production for go-online

--- program/2_牛刀小试/2_Pandas初阶/work2.2.py
+++ program/2_牛刀小试/2_Pandas初阶/work2.2.py
@@ -5,4 +5,6 @@ import datetime
 from dateutil.relativedelta import relativedelta
 
 
-# df['candle_begin_ti
+# df['candle_begin_time_GMT8'] = pd.to_datetime(df['candle_begin_time'], unit='ms') + timedelta(hours=8)  # 北京时间
+# df.drop(columns=['candle_begin_time'], inplace=True)
+# df = df[['candle_begin_time_GMT8', 'open', 'high', 'low', 'close'
