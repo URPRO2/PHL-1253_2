5f8771cdb5d8e9d6080e0f1ccd6356e2615c50ef
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-11-30 08:41:50 +0000

    remove unnecessary class

--- program/2_牛刀小试/3_构建自己的数字货币数据库/1_神奇的网址-什么是API.py
+++ program/2_牛刀小试/3_构建自己的数字货币数据库/1_神奇的网址-什么是API.py
@@ -1,3 +1,15 @@
 """
 《邢不行-2020新版|Python数字货币量化投资课程》
-无需编程基础，助教答疑服务
+无需编程基础，助教答疑服务，专属策略网站，一旦加入，永续更新。
+课程详细介绍：https://quantclass.cn/crypto/class
+邢不行微信: xbx9025
+本程序作者: 邢不行
+
+# 课程内容
+通过案例介绍什么是API
+"""
+
+
+# =====神奇的网址-API示例
+# okex：https://www.okex.com/api/spot/v3/instruments/BTC-USDT/ticker
+# 返
