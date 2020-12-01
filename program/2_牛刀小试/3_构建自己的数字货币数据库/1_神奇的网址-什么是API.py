bf4347099907927eea7536a1cebcb899a1b55b9a
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-12-01 19:47:42 +0000

    Change comment

--- program/2_牛刀小试/3_构建自己的数字货币数据库/1_神奇的网址-什么是API.py
+++ program/2_牛刀小试/3_构建自己的数字货币数据库/1_神奇的网址-什么是API.py
@@ -15,4 +15,6 @@
 # 返回结果示例：{"best_ask":"7871.9","best_bid":"7871.8","instrument_id":"BTC-USDT","product_id":"BTC-USDT","last":"7872.7","last_qty":"0.00294821","ask":"7871.9","best_ask_size":"2.47032541","bid":"7871.8","best_bid_size":"0.3586146","open_24h":"8090.4","high_24h":"8090.4","low_24h":"7637.4","base_volume_24h":"71999.67613995","timestamp":"2020-03-10T03:27:31.069Z","quote_volume_24h":"564510343.1"}
 
 # 火币：https://api.huobi.pro/market/detail/merged?symbol=btcusdt
-# 返回结果示例：{"status":"ok","ch":"market.btcusdt.detail.merged","ts":1583810974164,"tick":{"amount":71311.9480485466
+# 返回结果示例：{"status":"ok","ch":"market.btcusdt.detail.merged","ts":1583810974164,"tick":{"amount":71311.94804854663,"open":8082.13,"close":7890.19,"high":8082.14,"id":210146022322,"count":561789,"low":7638.0,"version":210146022322,"ask":[7890.98,0.58188],"vol":5.587285592033827E8,"bid":[7888.83,0.061314]}}
+
+# 币安：https://api.binance.c
