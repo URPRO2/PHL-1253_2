8fac98801330a764df7b73f97237e198b77d2818
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-12-03 21:43:05 +0000

    Fixed ucerf

--- program/2_牛刀小试/4_数字货币自动交易/1_如何创建API秘钥.py
+++ program/2_牛刀小试/4_数字货币自动交易/1_如何创建API秘钥.py
@@ -10,4 +10,16 @@
 """
 import pandas as pd
 import ccxt
-pd.s
+pd.set_option('expand_frame_repr', False)  # 当列太多时不换行
+
+
+# 创建ccxt的okex交易所
+exchange = ccxt.okex3()
+
+# 设置API
+exchange.apiKey = ''
+exchange.secret = ''
+exchange.password = ''  # okex在创建第三代api的时候，需要填写一个Passphrase。这个填写到这里即可
+
+# 获取账户信息
+balan
