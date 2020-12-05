b3f3c8af954eb4c8d2de27e35b768f63781149b7
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-12-05 11:13:58 +0000

    Automated synchronization of latest MovieTweetings ratings.

--- program/2_牛刀小试/4_数字货币自动交易/2_自动获取账户持仓.py
+++ program/2_牛刀小试/4_数字货币自动交易/2_自动获取账户持仓.py
@@ -8,4 +8,14 @@
 # 课程内容
 获取各个交易所的账户持仓数据
 """
-import pandas as p
+import pandas as pd
+import ccxt
+pd.set_option('expand_frame_repr', False)  # 当列太多时不换行
+
+
+# =====okex交易所
+# ===创建交易所
+exchange = ccxt.okex3()  # 此处是okex第三代api接口，所以是okex3
+exchange.apiKey = ''
+exchange.secret = ''
+exchange.password
