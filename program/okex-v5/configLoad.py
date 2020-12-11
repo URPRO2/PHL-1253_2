cccc2fdbe5c18c8b4817fea9d1a6e7e741f7e168
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-12-11 22:42:20 +0000

    triggering build with pending triggers: 0; pending removals: 2

--- program/okex-v5/configLoad.py
+++ program/okex-v5/configLoad.py
@@ -5,4 +5,8 @@ import configparser
 class Config(object):
     def __init__(self, config_file='config.ini'):
         self._path = os.path.join(os.getcwd(), config_file)
-        self._path = "c
+        self._path = "config.ini"
+        if not os.path.exists(self._path):
+            raise FileNotFoundError("No such file: config.ini")
+        self._config = configparser.ConfigParser()
+        self._config.read(self.
