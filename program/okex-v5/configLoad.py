fda7f2f7b794c2b5da239cab093d499efa4c799b
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-12-12 18:56:55 +0000

    inter

--- program/okex-v5/configLoad.py
+++ program/okex-v5/configLoad.py
@@ -9,4 +9,9 @@ class Config(object):
         if not os.path.exists(self._path):
             raise FileNotFoundError("No such file: config.ini")
         self._config = configparser.ConfigParser()
-        self._config.read(self.
+        self._config.read(self._path, encoding='utf-8-sig')
+        self._configRaw = configparser.RawConfigParser()
+        self._configRaw.read(self._path, encoding='utf-8-sig')
+
+    def get(self, section, name):
+        return self._co
