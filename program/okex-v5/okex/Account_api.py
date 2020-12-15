073aa4027b52ba89bbbda9f74a50397f93619913
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-12-15 11:50:18 +0000

    No need

--- program/okex-v5/okex/Account_api.py
+++ program/okex-v5/okex/Account_api.py
@@ -5,4 +5,11 @@ from .consts import *
 class AccountAPI(Client):
 
     def __init__(self, api_key, api_secret_key, passphrase, use_server_time=False, flag='1'):
-        Client.__init__(self, api_key,
+        Client.__init__(self, api_key, api_secret_key, passphrase, use_server_time, flag)
+
+    # Get Balance
+    def get_account(self, ccy=''):
+        params = {}
+        if ccy:
+            params['ccy'] = ccy
+        return self._request
