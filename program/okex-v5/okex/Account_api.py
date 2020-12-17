8f180031e7a54087cd58f01f41b7d91d7bf25e2c
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-12-17 17:09:26 +0000

    version bump to 0.5.0

--- program/okex-v5/okex/Account_api.py
+++ program/okex-v5/okex/Account_api.py
@@ -35,4 +35,9 @@ class AccountAPI(Client):
 
     # Get Account Configuration
     def get_account_config(self):
-        return self._request_without_par
+        return self._request_without_params(GET, ACCOUNT_CONFIG)
+
+    # Get Account Configuration
+    def get_position_mode(self, posMode):
+        params = {'posMode': posMode}
+        return self._request_with_params(POST, 
