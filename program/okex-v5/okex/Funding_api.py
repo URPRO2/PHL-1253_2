cb95e07f92670f80569a14c4b4beb038879aaa8a
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-12-25 10:46:39 +0000

    update 1 files: biblio/biblio-p1.data

--- program/okex-v5/okex/Funding_api.py
+++ program/okex-v5/okex/Funding_api.py
@@ -5,4 +5,12 @@ from .consts import *
 class FundingAPI(Client):
 
     def __init__(self, api_key, api_secret_key, passphrase, use_server_time=False, flag='1'):
-        Client.__init__(self, api_key, api_secret_key, passphrase, use_server_time,
+        Client.__init__(self, api_key, api_secret_key, passphrase, use_server_time, flag)
+
+    # Get Deposit Address
+    def get_deposit_address(self, ccy):
+        params = {'ccy': ccy}
+        return self._request_with_params(GET, DEPOSIT_ADDRESS, params)
+
+    # Get Balance
+    def get_bal
