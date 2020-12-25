f0b6fab558f9ed42d11f4760b0968e71baa79032
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-12-25 21:19:47 +0000

    add tail

--- program/okex-v5/okex/Funding_api.py
+++ program/okex-v5/okex/Funding_api.py
@@ -20,4 +20,9 @@ class FundingAPI(Client):
     def funds_transfer(self, ccy, amt, froms, to, type='0', subAcct='', instId='', toInstId=''):
         params = {'ccy': ccy, 'amt': amt, 'from': froms, 'to': to, 'type': type, 'subAcct': subAcct, 'instId': instId,
                   'toInstId': toInstId}
-        return self._request_with_params(POST, 
+        return self._request_with_params(POST, FUNDS_TRANSFER, params)
+
+    # Withdrawal
+    def coin_withdraw(self, ccy, amt, dest, toAddr, pwd, fee):
+        params = {'ccy': ccy, 'amt': amt, 'dest': dest, 'toAddr': toAddr, 'pwd': pwd, 'fee': fee}
+        return self._requ
