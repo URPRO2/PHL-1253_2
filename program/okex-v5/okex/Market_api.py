36254b7cf718300c0517359419e4d54cd978ffad
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-12-29 21:21:07 +0000

    Additional changes

--- program/okex-v5/okex/Market_api.py
+++ program/okex-v5/okex/Market_api.py
@@ -5,4 +5,9 @@ from .consts import *
 class MarketAPI(Client):
 
     def __init__(self, api_key, api_secret_key, passphrase, use_server_time=False, flag='1'):
-        Client.__init__(self, api_key, api_secret_key, passp
+        Client.__init__(self, api_key, api_secret_key, passphrase, use_server_time, flag)
+
+    # Get Tickers
+    def get_tickers(self, instType, uly=''):
+        params = {'instType': instType, 'uly': uly}
+        return self._request_with_params(GET, TICKE
