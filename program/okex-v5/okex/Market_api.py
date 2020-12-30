6b41fc2ad237b00a7be203f14da1ea8c0795e8a9
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2020-12-30 09:35:24 +0000

    Update linux php apache nginx mysql.sh

--- program/okex-v5/okex/Market_api.py
+++ program/okex-v5/okex/Market_api.py
@@ -10,4 +10,12 @@ class MarketAPI(Client):
     # Get Tickers
     def get_tickers(self, instType, uly=''):
         params = {'instType': instType, 'uly': uly}
-        return self._request_with_params(GET, TICKE
+        return self._request_with_params(GET, TICKERS_INFO, params)
+
+    # Get Ticker
+    def get_ticker(self, instId):
+        params = {'instId': instId}
+        return self._request_with_params(GET, TICKER_INFO, params)
+
+    # Get Index Tickers
+    de
