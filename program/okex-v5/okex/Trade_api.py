5d1799908eed0bedee513456cb1e8bad42027ee5
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2021-01-02 20:42:38 +0000

    Add contributing.md

--- program/okex-v5/okex/Trade_api.py
+++ program/okex-v5/okex/Trade_api.py
@@ -4,4 +4,8 @@ from .consts import *
 
 class TradeAPI(Client):
 
-    def __init__(self, api_key, api_secret_key, passphrase, use_ser
+    def __init__(self, api_key, api_secret_key, passphrase, use_server_time=False, flag='1'):
+        Client.__init__(self, api_key, api_secret_key, passphrase, use_server_time, flag)
+
+    # Place Order
+    def place_order(self, instId, tdMode, side, ordType, sz, ccy
