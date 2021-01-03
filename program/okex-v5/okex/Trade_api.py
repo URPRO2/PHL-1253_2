5f9ee6844d4424e8f4f7db66ad407995fd747dcd
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2021-01-03 05:40:45 +0000

    fixing error

--- program/okex-v5/okex/Trade_api.py
+++ program/okex-v5/okex/Trade_api.py
@@ -8,4 +8,7 @@ class TradeAPI(Client):
         Client.__init__(self, api_key, api_secret_key, passphrase, use_server_time, flag)
 
     # Place Order
-    def place_order(self, instId, tdMode, side, ordType, sz, ccy
+    def place_order(self, instId, tdMode, side, ordType, sz, ccy='', clOrdId='', tag='', posSide='', px='',
+                    reduceOnly=''):
+        params = {'instId': instId, 'tdMode': tdMode, 'side': side, 'ordType': ordType, 'sz': sz, 'ccy': ccy,
+   
