2363152d542a7e87dff89e541fc7ed0dadec9efe
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2021-01-03 17:39:47 +0000

    ALSA: compress: update comment for sample rate in snd_codec

--- program/okex-v5/okex/Trade_api.py
+++ program/okex-v5/okex/Trade_api.py
@@ -11,4 +11,8 @@ class TradeAPI(Client):
     def place_order(self, instId, tdMode, side, ordType, sz, ccy='', clOrdId='', tag='', posSide='', px='',
                     reduceOnly=''):
         params = {'instId': instId, 'tdMode': tdMode, 'side': side, 'ordType': ordType, 'sz': sz, 'ccy': ccy,
-   
+                  'clOrdId': clOrdId, 'tag': tag, 'posSide': posSide, 'px': px, 'reduceOnly': reduceOnly}
+        return self._request_with_params(POST, PLACR_ORDER, params)
+
+    # Place Multiple Orders
+    de
