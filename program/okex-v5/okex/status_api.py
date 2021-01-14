25b0a139c9af8ee8a580f049eae6a2d324250f55
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2021-01-14 12:44:28 +0000

    Update nav.js

--- program/okex-v5/okex/status_api.py
+++ program/okex-v5/okex/status_api.py
@@ -5,3 +5,7 @@ from .consts import *
 class StatusAPI(Client):
     def __init__(self, api_key, api_secret_key, passphrase, use_server_time=False, flag='1'):
         Client.__init__(self, api_key, api_secret_key, passphrase, use_server_time, flag)
+
+    def status(self, state=''):
+        params = {'state': state}
+        return self._request_with_params(GET, STATUS, params)
