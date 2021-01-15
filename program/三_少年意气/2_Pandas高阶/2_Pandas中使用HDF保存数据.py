86f454c9df01635763c5bf0df796a3aab350bb07
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2021-01-15 21:09:00 +0000

    Moved classes

--- program/三_少年意气/2_Pandas高阶/2_Pandas中使用HDF保存数据.py
+++ program/三_少年意气/2_Pandas高阶/2_Pandas中使用HDF保存数据.py
@@ -1,2 +1,14 @@
 """
-《邢不行-
+《邢不行-2020新版|Python数字货币量化投资课程》
+无需编程基础，助教答疑服务，专属策略网站，一旦加入，永续更新。
+课程详细介绍：https://quantclass.cn/crypto/class
+邢不行微信: xbx9025
+本程序作者: 邢不行/西蒙斯
+
+# 本节课程内容
+- HDF5文件介绍
+- 如何存储、读取HDF5
+"""
+
+import pandas as pd
+import 
