efa4826ba799781fc0f581740e67fc11c8aa3696
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2021-01-19 12:00:41 +0000

    Locales

--- program/三_少年意气/2_Pandas高阶/4_Pandas分组处理操作-groupby.py
+++ program/三_少年意气/2_Pandas高阶/4_Pandas分组处理操作-groupby.py
@@ -26,4 +26,11 @@ df = pd.read_csv(r'C:\Users\Simons\Desktop\xbx_coin_2020\data\cls-3.2BITFINEX-1H
 # 根据'candle_begin_time'进行group，将相同'交易日期'的行放入一个group，
 # print(df.groupby('candle_begin_time'))  # 生成一个group对象。不会做实质性操作，只是会判断是否可以根据该变量进行groupby
 
-# gr
+# group后可以使用相关函数，size()计算每个group的行数
+# print(df.groupby('candle_begin_time').size())  # 每小时交易的币的个数
+# 根据'symbol'进行group，将相同'symbol'的行放入一个group，
+# print(df.groupby('symbol').size())  # 每个币交易的小时数
+
+
+# 获取其中某一个group
+# print(df.
