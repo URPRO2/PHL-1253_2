cb7d5ddce6e27db6db1d2ecf626ef2cbee38dc68
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2021-02-02 21:13:12 +0000

    Update general.js

--- program/三_少年意气/3_择时策略开发与回测/2_产生策略信号.py
+++ program/三_少年意气/3_择时策略开发与回测/2_产生策略信号.py
@@ -14,4 +14,9 @@ pd.set_option('expand_frame_repr', False)  # 当列太多时不换行
 
 # =====读入数据
 symbol = 'BTC-USDT_5m'
-df = pd.read_hdf('/Users/xingbuxingx/Desktop/数字货币量化课程/2020版数字货币
+df = pd.read_hdf('/Users/xingbuxingx/Desktop/数字货币量化课程/2020版数字货币量化投资课程/xbx_coin_2020/data/%s.h5' % symbol, key='df')
+
+# 任何原始数据读入都进行一下排序、去重，以防万一
+df.sort_values(by=['candle_begin_time'], inplace=True)
+df.drop_duplicates(subset=['candle_begin_time'], inplace=True)
+df.
