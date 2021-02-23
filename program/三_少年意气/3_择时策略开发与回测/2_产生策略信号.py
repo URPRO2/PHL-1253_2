c362a5bdbc82287ff1e486f5e0e2597d5bcb71a5
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2021-02-23 05:45:15 +0000

    Fix wrong method name

--- program/三_少年意气/3_择时策略开发与回测/2_产生策略信号.py
+++ program/三_少年意气/3_择时策略开发与回测/2_产生策略信号.py
@@ -78,4 +78,8 @@ df.loc[condition1 & condition2, 'signal_short'] = -1  # 将产生做空信号的
 
 # 找出做空平仓信号
 condition1 = df['close'] > df['median']  # 当前K线的收盘价 > 中轨
-condition2 = df['close'].shift(1) <= df[
+condition2 = df['close'].shift(1) <= df['median'].shift(1)  # 之前K线的收盘价 <= 中轨
+df.loc[condition1 & condition2, 'signal_short'] = 0  # 将产生平仓信号当天的signal设置为0，0代表平仓
+
+# 合并做多做空信号，去除重复信号
+df['signal'] = df[['signal_long', 'signal_short']].sum(axis=1, min_count=
