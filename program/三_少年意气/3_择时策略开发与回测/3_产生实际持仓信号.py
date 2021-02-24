9e7f13080837b7fdf2772be60133639f5303ce7f
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2021-02-24 12:48:15 +0000

    commit new files

--- program/三_少年意气/3_择时策略开发与回测/3_产生实际持仓信号.py
+++ program/三_少年意气/3_择时策略开发与回测/3_产生实际持仓信号.py
@@ -1,3 +1,13 @@
 """
 《邢不行-2020新版|Python数字货币量化投资课程》
-无需编程基础，助教答疑服务，专属策略网站
+无需编程基础，助教答疑服务，专属策略网站，一旦加入，永续更新。
+课程详细介绍：https://quantclass.cn/crypto/class
+邢不行微信: xbx9025
+本程序作者: 邢不行
+
+# 课程内容
+介绍如何将策略信号signal，转换成实际持仓pos
+"""
+import pandas as pd
+pd.set_option('expand_frame_repr', False)  # 当列太多时不换行
+pd.set
