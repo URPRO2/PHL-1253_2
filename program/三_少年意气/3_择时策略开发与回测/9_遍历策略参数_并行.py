8581858a1412635741e1823b2e42b07f3f941690
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2021-03-12 11:09:36 +0000

    Update postgres

--- program/三_少年意气/3_择时策略开发与回测/9_遍历策略参数_并行.py
+++ program/三_少年意气/3_择时策略开发与回测/9_遍历策略参数_并行.py
@@ -50,4 +50,7 @@ period_df = df.resample(rule=rule_type, on='candle_begin_time', label='left', cl
      'quote_volume': 'sum',
      })
 period_df.dropna(subset=['open'], inplace=True)  # 去除一天都没有交易的周期
-period_df = period_df[period_d
+period_df = period_df[period_df['volume'] > 0]  # 去除成交量为0的交易周期
+period_df.reset_index(inplace=True)
+df = period_df[['candle_begin_time', 'open', 'high', 'low', 'close', 'volume', 'quote_volume']]
+df = df[df['candle_begin_time'] 
