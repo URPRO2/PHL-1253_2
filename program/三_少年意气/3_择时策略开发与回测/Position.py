94c7c01fe1aea8410de518c42760c5a1b14e7199
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2021-03-21 19:44:18 +0000

    ASoC: wm9712: Fix microphone source selection

--- program/三_少年意气/3_择时策略开发与回测/Position.py
+++ program/三_少年意气/3_择时策略开发与回测/Position.py
@@ -1,3 +1,17 @@
 """
 《邢不行-2020新版|Python数字货币量化投资课程》
-无需编程基础，助教答疑服务，
+无需编程基础，助教答疑服务，专属策略网站，一旦加入，永续更新。
+课程详细介绍：https://quantclass.cn/crypto/class
+邢不行微信: xbx9025
+本程序作者: 邢不行
+
+# 课程内容
+计算实际持仓的函数
+"""
+import pandas as pd
+
+
+# 由交易信号产生实际持仓
+def position_for_OKEx_future(df):
+    """
+    根据signa
