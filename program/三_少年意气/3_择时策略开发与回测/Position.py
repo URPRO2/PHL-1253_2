358b59524a13e87dab616a1a446bb844d1c3e7c8
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2021-03-25 14:40:50 +0000

    time change

--- program/三_少年意气/3_择时策略开发与回测/Position.py
+++ program/三_少年意气/3_择时策略开发与回测/Position.py
@@ -33,4 +33,12 @@ def position_for_OKEx_future(df):
     condition = (df['candle_begin_time'].dt.hour == 16) & (df['candle_begin_time'].dt.minute == 0)
     df.loc[condition, 'pos'] = None
     # pos为空的时，不能买卖，只能和前一周期保持一致。
-    d
+    df['pos'].fillna(method='ffill', inplace=True)
+
+    # 在实际操作中，不一定会直接跳过4点这个周期，而是会停止N分钟下单。此时可以注释掉上面的代码。
+
+    # ===将数据存入hdf文件中
+    # 删除无关中间变量
+    df.drop(['signal'], axis=1, inplace=True)
+
+    return df
