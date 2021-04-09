e67937a39e17407b95ba7e36dd58c515e822a7bd
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2021-04-09 22:42:43 +0000

    remove compiled files

--- program/三_少年意气/四_择时策略实盘初阶/Function.py
+++ program/三_少年意气/四_择时策略实盘初阶/Function.py
@@ -1,3 +1,17 @@
 """
 《邢不行-2020新版|Python数字货币量化投资课程》
-无需编程基础，助教答疑服务，专属策略网站，一旦加入，
+无需编程基础，助教答疑服务，专属策略网站，一旦加入，永续更新。
+课程详细介绍：https://quantclass.cn/crypto/class
+邢不行微信: xbx9025
+本程序作者: 邢不行
+
+# 课程内容
+择时策略实盘需要的相关函数
+"""
+import ccxt
+import math
+import time
+import pandas as pd
+from datetime import datetime, timedelta
+import json
+impo
