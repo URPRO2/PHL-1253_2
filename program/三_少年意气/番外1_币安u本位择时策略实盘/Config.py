450648824258b215c46242c760cd8898fb4cbfd2
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2021-12-11 15:12:05 +0000

    Updating build-info/dotnet/coreclr/master for stable

--- program/三_少年意气/番外1_币安u本位择时策略实盘/Config.py
+++ program/三_少年意气/番外1_币安u本位择时策略实盘/Config.py
@@ -10,4 +10,16 @@
 """
 # sleep时间配置
 short_sleep_time = 1  # 用于和交易所交互时比较紧急的时间sleep，例如获取数据、下单
-medium_sleep_time = 2  #
+medium_sleep_time = 2  # 用于和交易所交互时不是很紧急的时间sleep，例如获取持仓
+long_sleep_time = 10  # 用于较长的时间sleep
+
+# timeout时间
+exchange_timeout = 3000  # 3s
+
+# 订单对照表
+binance_order_type = {
+    '开多': 'BUY',
+    '开空': 'SELL',
+    '平多': 'SELL',
+    '平空': 'BUY',
+    '平空
