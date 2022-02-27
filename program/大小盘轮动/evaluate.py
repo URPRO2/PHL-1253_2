a0af60d96da3066c81fcf465c4f372081438017e
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-02-27 21:12:04 +0000

    Ignore gem files

--- program/大小盘轮动/evaluate.py
+++ program/大小盘轮动/evaluate.py
@@ -8,4 +8,14 @@ def equity_curve_for_OKEx_USDT_future_next_open(df, slippage=1/1000,c_rate=5/100
     计算持仓时能买入多少张
     计算买入后剩余的钱（扣除买入手续费）
     计算卖出的手续费
-    计算盈亏（滑点的
+    计算盈亏（滑点的计算）
+    计算净值 （收盘价）
+    计算最小最大净值（最高最低）计算爆仓用
+    """
+
+    df['next_open'] = df['open'].shift(-1)
+    df['next_open'].fillna(value=df['close'],inplace=True)
+
+    #开仓（开空，开多）K线
+    condition1 = df['pos'] != 0
+    co
