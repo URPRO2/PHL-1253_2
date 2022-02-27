014ebd980dc641b2e2eaa84f43e56d2a316d5660
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-02-27 10:06:05 +0000

    merge from gcc

--- program/大小盘轮动/evaluate.py
+++ program/大小盘轮动/evaluate.py
@@ -2,4 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-def equity_curve_for_OKEx_
+def equity_curve_for_OKEx_USDT_future_next_open(df, slippage=1/1000,c_rate=5/10000,leverage_rate=3,face_value=0.01,min_margin_ratio=1/100):
+    """
+    计算持仓的开始时间
+    计算持仓时能买入多少张
+    计算买入后剩余的钱（扣除买入手续费）
+    计算卖出的手续费
+    计算盈亏（滑点的
