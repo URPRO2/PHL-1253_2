4e58c8a07e5d754041659035f5e8dae634769157
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-03-27 21:03:52 +0000

    update redis

--- program/大小盘轮动/evaluate.py
+++ program/大小盘轮动/evaluate.py
@@ -81,4 +81,9 @@ def equity_curve_for_OKEx_USDT_future_next_open(df, slippage=1/1000,c_rate=5/100
 
     # =====删除不必要的数据，并存储
     df.drop(['next_open', 'contract_num', 'open_pos_price', 'cash', 'close_pos_price', 'close_pos_fee',
-  
+             'profit', 'net_value', 'price_min', 'profit_min', 'net_value_min', 'margin_ratio', '是否爆仓'],
+            axis=1, inplace=True)
+
+    print(df)
+    return df
+
