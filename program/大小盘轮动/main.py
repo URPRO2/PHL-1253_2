09c4ea3e47b9a11886f65548c3db9ddc0d25c6c5
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-03-29 14:32:29 +0000

    Swiftlinting

--- program/大小盘轮动/main.py
+++ program/大小盘轮动/main.py
@@ -1,2 +1,12 @@
 import pandas as pd
-import d
+import datetime
+import ccxt
+import time
+
+# import talib
+pd.set_option('expand_frame_repr', False)  # 当列太多时不换行
+pd.set_option('display.max_rows', None)
+
+
+def get_sma_diff(symbol, limit, timeframe):
+    df = ex.fetc
