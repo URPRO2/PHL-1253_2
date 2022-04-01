25a7400c7f733ee0fb4247d9182fcac592a7fcec
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-04-01 14:03:12 +0000

    A little change

--- program/大小盘轮动/main.py
+++ program/大小盘轮动/main.py
@@ -9,4 +9,6 @@ pd.set_option('display.max_rows', None)
 
 
 def get_sma_diff(symbol, limit, timeframe):
-    df = ex.fetc
+    df = ex.fetch_ohlcv(symbol=symbol, timeframe=timeframe, limit=20)
+    df = pd.DataFrame(df, dtype=float)
+    df.rename(columns={0: 'candle_begin_time', 1: 'open', 2: 'high', 3: 'low', 4: 'close', 5: 'volume'}, inplace=True)
