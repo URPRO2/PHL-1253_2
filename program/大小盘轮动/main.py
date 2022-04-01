71fd7236e83c92ef68612c1bd8d2be7d06628149
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-04-01 20:46:24 +0000

    Docu update

--- program/大小盘轮动/main.py
+++ program/大小盘轮动/main.py
@@ -12,3 +12,9 @@ def get_sma_diff(symbol, limit, timeframe):
     df = ex.fetch_ohlcv(symbol=symbol, timeframe=timeframe, limit=20)
     df = pd.DataFrame(df, dtype=float)
     df.rename(columns={0: 'candle_begin_time', 1: 'open', 2: 'high', 3: 'low', 4: 'close', 5: 'volume'}, inplace=True)
+    # sma = signal_sma(df, limit)[-1]
+    # sma = talib.SMA(df['close'].values, limit)[-1]
+    sma = df['close'].mean()
+
+    df['diff'] = df['close'].pct_change(19)
+    # ret = (df.iat[-1,
