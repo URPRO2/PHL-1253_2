4b54631928cded37b96911c403182e4f81d6da69
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-05-05 07:51:59 +0000

    dm-verity: run in a WQ_HIGHPRI workqueue

--- program/大小盘轮动/position.py
+++ program/大小盘轮动/position.py
@@ -4,4 +4,14 @@
 import pandas as pd
 
 # 由交易信号产生实际持仓
-def position_for_OKEx_future
+def position_for_OKEx_future(df):
+    """
+    根据signal产生实际持仓。考虑各种不能买入卖出的情况。
+    所有的交易都是发生在产生信号的K线的结束时
+    :param df:
+    :return:
+    """
+
+    # ===由signal计算出实际的每天持有仓位
+    # 在产生signal的k线结束的时候，进行买入
+    df['signal'].fillna(met
