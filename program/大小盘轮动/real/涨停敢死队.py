714c511d1d208cb469b1667d6a8033bb39566904
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-05-17 22:52:31 +0000

    Fixed Typo

--- program/大小盘轮动/real/涨停敢死队.py
+++ program/大小盘轮动/real/涨停敢死队.py
@@ -10,4 +10,10 @@
 """
 import ccxt
 from time import sleep
-import p
+import pandas as pd
+from datetime import datetime
+
+pd.set_option('display.max_rows', 1000)
+pd.set_option('expand_frame_repr', False)  # 当列太多时不换行
+# 设置命令行输出时的列对齐功能
+pd.set_option('display.unicode.ambiguous_as_wide', 
