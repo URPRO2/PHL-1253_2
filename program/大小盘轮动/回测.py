1baa0d60b2ed80e02a97a0268ea0bc9d282bbbe7
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-08-07 09:37:58 +0000

    fs/super.c: sync ro remount after blocking writers

--- program/大小盘轮动/回测.py
+++ program/大小盘轮动/回测.py
@@ -2,4 +2,10 @@
 在此之前需要行情数据，备份
 回测大小盘（BTC、ETH）
 """
-import pa
+import pandas as pd
+from Tool import *
+pd.set_option('display.max_rows', 1000)
+pd.set_option('expand_frame_repr', False)  # 当列太多时不换行
+# 设置命令行输出时的列对齐功能
+pd.set_option('display.unicode.ambiguous_as_wide', True)
+pd.set_option
