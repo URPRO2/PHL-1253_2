8ce62b87925a9b914fe88486517626f77489f296
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-08-07 18:35:53 +0000

    Fix typo in test name

--- program/大小盘轮动/回测.py
+++ program/大小盘轮动/回测.py
@@ -8,4 +8,11 @@ pd.set_option('display.max_rows', 1000)
 pd.set_option('expand_frame_repr', False)  # 当列太多时不换行
 # 设置命令行输出时的列对齐功能
 pd.set_option('display.unicode.ambiguous_as_wide', True)
-pd.set_option
+pd.set_option('display.unicode.east_asian_width', True)
+
+
+# ==========整理出历史数据、转换4小时数据============
+btcDf = getSymbolData('BTC-USDT_5m')
+ethDf = getSymbolData('ETH-USDT_5m')
+print(btcDf)
+# btcDf['mean120'] = btcDf['cl
