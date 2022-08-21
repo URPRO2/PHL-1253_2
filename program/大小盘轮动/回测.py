1246a89a89344027e1a9c8e93f0553f2ba552edb
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-08-21 12:08:36 +0000

    Updated .podspec

--- program/大小盘轮动/回测.py
+++ program/大小盘轮动/回测.py
@@ -49,4 +49,9 @@ df_merged.drop(['signal'], axis=1, inplace=True)
 
 # =====找出开仓、平仓的k线
 condition1 = df_merged['pos'] != 0  # 当前周期不为空仓
-condition2 = df_merged['pos'] != df_merged['pos'].sh
+condition2 = df_merged['pos'] != df_merged['pos'].shift(1)  # 当前周期和上个周期持仓方向不一样。
+open_pos_condition = condition1 & condition2
+
+condition1 = df_merged['pos'] != 0  # 当前周期不为空仓
+condition2 = df_merged['pos'] != df_merged['pos'].shift(-1)  # 当前周期和下个周期持仓方向不一样。
+close_po
