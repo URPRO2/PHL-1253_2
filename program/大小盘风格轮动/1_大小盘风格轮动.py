bea290de8738fbd547a54f2969d5e05cab7e8871
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-08-28 15:44:23 +0000

    fill missing fields

--- program/大小盘风格轮动/1_大小盘风格轮动.py
+++ program/大小盘风格轮动/1_大小盘风格轮动.py
@@ -1,1 +1,10 @@
 '''
+邢不行 | 量化小讲堂系列文章
+《抱团股会一直涨？无脑执行大小盘轮动策略，轻松跑赢指数5倍【附Python代码】》
+https://mp.weixin.qq.com/s/hPjVbBKomfMhowc32jUwhA
+获取更多量化文章，请联系邢不行个人微信：xbx3642
+'''
+import pandas as pd
+import numpy as np
+from function import *
+import matplotlib.pyplo
