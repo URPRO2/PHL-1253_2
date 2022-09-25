ff2f50f530ee4ea29992633eeea51e10cda263f1
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-09-25 23:01:51 +0000

    authors

--- program/大小盘风格轮动/function.py
+++ program/大小盘风格轮动/function.py
@@ -19,4 +19,8 @@ def evaluate_investment(source_data, tittle,time='交易日期'):
             '1 days 00:00:00' / (temp[time].iloc[-1] - temp[time].iloc[0]) * 365) - 1
     results.loc[0, '年化收益'] = str(round(annual_return * 100, 2)) + '%'
 
-    # ===计算最大回撤，最大
+    # ===计算最大回撤，最大回撤的含义：《如何通过3行代码计算最大回撤》https://mp.weixin.qq.com/s/Dwt4lkKR_PEnWRprLlvPVw
+    # 计算当日之前的资金曲线的最高点
+    temp['max2here'] = temp[tittle].expanding().max()
+    # 计算到历史最高值到当日的跌幅，drowdwon
+    temp['d
