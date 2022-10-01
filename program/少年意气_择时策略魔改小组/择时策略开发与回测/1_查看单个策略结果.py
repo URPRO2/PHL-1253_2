5177aaf80a02f438a58c3a05442f13f3c7f3c84f
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-10-01 17:27:41 +0000

    ixgbe: Do not pad FCoE frames as this can cause issues with FCoE DDP

--- program/少年意气_择时策略魔改小组/择时策略开发与回测/1_查看单个策略结果.py
+++ program/少年意气_择时策略魔改小组/择时策略开发与回测/1_查看单个策略结果.py
@@ -1,3 +1,11 @@
 """
 《邢不行-2020新版|Python数字货币量化投资课程》
-择时策略魔改研究小组（
+择时策略魔改研究小组（第1期）
+无需编程基础，助教答疑服务，专属策略网站，一旦加入，永续更新。
+课程详细介绍：https://quantclass.cn/crypto/class
+邢不行微信: xbx9585
+本程序作者: 邢不行
+"""
+
+from datetime import timedelta
+from program.少年意气_择时策略魔改小组.择时策略开发与回测 impor
