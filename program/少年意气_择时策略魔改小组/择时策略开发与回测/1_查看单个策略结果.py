b79927b9eb81d51b95078014006cc97ed462e9e6
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-10-02 21:07:41 +0000

    libata: add ATA_HORKAGE_BROKEN_FPDMA_AA quirk for Seagate Momentus SpinPoint M8

--- program/少年意气_择时策略魔改小组/择时策略开发与回测/1_查看单个策略结果.py
+++ program/少年意气_择时策略魔改小组/择时策略开发与回测/1_查看单个策略结果.py
@@ -12,4 +12,12 @@ from program.少年意气_择时策略魔改小组.择时策略开发与回测 i
 from program.少年意气_择时策略魔改小组.择时策略开发与回测.Position import *
 from program.少年意气_择时策略魔改小组.择时策略开发与回测.Evaluate import *
 from program.少年意气_择时策略魔改小组.择时策略开发与回测.Function import *
-from program.少年意气_择时策略魔
+from program.少年意气_择时策略魔改小组.择时策略开发与回测.Statistics import *
+
+pd.set_option('expand_frame_repr', False)  # 当列太多时不换行
+pd.set_option('display.max_rows', 500)  # 最多显示数据的行数
+
+# =====手工设定策略参数
+symbol = 'BTC-USDT_5m'
+para = [220, 2.3]
+signal_name = 'signal_m
