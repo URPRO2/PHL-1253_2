d0d1dfc9f0ba4f5a8a0706efe05f6eaf3388bf60
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-10-15 12:15:56 +0000

    bump version to 1.1.3

--- program/少年意气_择时策略魔改小组/择时策略开发与回测/1_查看单个策略结果.py
+++ program/少年意气_择时策略魔改小组/择时策略开发与回测/1_查看单个策略结果.py
@@ -52,4 +52,7 @@ period_df = df.resample(rule=rule_type, on='candle_begin_time', label='left', cl
 period_df.dropna(subset=['open'], inplace=True)  # 去除一天都没有交易的周期
 period_df = period_df[period_df['volume'] > 0]  # 去除成交量为0的交易周期
 period_df.reset_index(inplace=True)
-df = period_df[['candle_begin_time', 'open', 'high', 'low', 'close', 'volume', 'quote_vol
+df = period_df[['candle_begin_time', 'open', 'high', 'low', 'close', 'volume', 'quote_volume', 'trade_num',
+                'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume']]
+df = df[df['candle_begin_time'] >= pd.to_datetime('2017-01-01')]
+df.reset_index(in
