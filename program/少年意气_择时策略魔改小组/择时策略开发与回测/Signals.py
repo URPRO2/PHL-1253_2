8f4c745ad4f85b306e743ac22d1c711c401089bc
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-11-09 21:06:47 +0000

    Improved error reporting

--- program/少年意气_择时策略魔改小组/择时策略开发与回测/Signals.py
+++ program/少年意气_择时策略魔改小组/择时策略开发与回测/Signals.py
@@ -45,4 +45,8 @@ def signal_simple_bolling(df, para=[200, 2]):
     df.loc[condition1 & condition2, 'signal_long'] = 1  # 将产生做多信号的那根K线的signal设置为1，1代表做多
 
     # 找出做多平仓信号
-    condition1 = df['clo
+    condition1 = df['close'] < df['median']  # 当前K线的收盘价 < 中轨
+    condition2 = df['close'].shift(1) >= df['median'].shift(1)  # 之前K线的收盘价 >= 中轨
+    df.loc[condition1 & condition2, 'signal_long'] = 0  # 将产生平仓信号当天的signal设置为0，0代表平仓
+
+    #
