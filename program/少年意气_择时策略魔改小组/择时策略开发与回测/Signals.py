e05d7d52b87aafdfa30fec8668a5f06f54b0fbe7
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-11-13 20:20:57 +0000

    Updated the twilio feedstock.

--- program/少年意气_择时策略魔改小组/择时策略开发与回测/Signals.py
+++ program/少年意气_择时策略魔改小组/择时策略开发与回测/Signals.py
@@ -52,4 +52,9 @@ def signal_simple_bolling(df, para=[200, 2]):
     # 找出做空信号
     condition1 = df['close'] < df['lower']  # 当前K线的收盘价 < 下轨
     condition2 = df['close'].shift(1) >= df['lower'].shift(1)  # 之前K线的收盘价 >= 下轨
-    df.loc[condition1 & condition2, 'signal_short'] = -1  # 将
+    df.loc[condition1 & condition2, 'signal_short'] = -1  # 将产生做空信号的那根K线的signal设置为-1，-1代表做空
+
+    # 找出做空平仓信号
+    condition1 = df['close'] > df['median']  # 当前K线的收盘价 > 中轨
+    condition2 = df['close'].shift(1) <= df['median'].shift(1)  # 之前K线的收盘价 <= 中轨
+    df.loc[condition1 & conditio
