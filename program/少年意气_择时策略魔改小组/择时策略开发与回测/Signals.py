7df7afe10092753249ffd128b174a358a11368a7
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-12-12 11:28:50 +0000

    Fix format string.

--- program/少年意气_择时策略魔改小组/择时策略开发与回测/Signals.py
+++ program/少年意气_择时策略魔改小组/择时策略开发与回测/Signals.py
@@ -228,4 +228,11 @@ def signal_my_bolling(df, para=[200, 2]):
     # 计算均线
     df['median'] = df['close'].rolling(n, min_periods=1).mean()
     # 计算上轨、下轨道
-    df['
+    df['std'] = df['close'].rolling(n, min_periods=1).std(ddof=0)  # ddof代表标准差自由度
+    df['upper'] = df['median'] + m * df['std']
+    df['lower'] = df['median'] - m * df['std']
+
+
+    # ===计算信号
+    # 找出做多信号
+    condition1 = df['close'] > df['
