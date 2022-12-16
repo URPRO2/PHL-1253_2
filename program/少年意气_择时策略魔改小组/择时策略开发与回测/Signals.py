88a097994458afdbc1aa36df9f069115b960ec06
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-12-16 21:16:55 +0000

    Add missing new line

--- program/少年意气_择时策略魔改小组/择时策略开发与回测/Signals.py
+++ program/少年意气_择时策略魔改小组/择时策略开发与回测/Signals.py
@@ -239,4 +239,7 @@ def signal_my_bolling(df, para=[200, 2]):
     condition2 = df['close'].shift(1) <= df['upper'].shift(1)  # 之前K线的收盘价 <= 上轨
     df.loc[condition1 & condition2, 'signal_long'] = 1  # 将产生做多信号的那根K线的signal设置为1，1代表做多
 
-    # 找出做多平
+    # 找出做多平仓信号
+    condition1 = df['close'] < df['median']  # 当前K线的收盘价 < 中轨
+    condition2 = df['close'].shift(1) >= df['median'].shift(1)  # 之前K线的收盘价 >= 中轨
+    df.loc[condition1 & condition2, 'signal_l
