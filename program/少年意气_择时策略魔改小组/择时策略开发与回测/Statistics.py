35cdcf4729529247818dc4ed7ed26869ca1936de
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2022-12-21 16:54:43 +0000

    Bump development version

--- program/少年意气_择时策略魔改小组/择时策略开发与回测/Statistics.py
+++ program/少年意气_择时策略魔改小组/择时策略开发与回测/Statistics.py
@@ -1,3 +1,16 @@
 """
 《邢不行-2020新版|Python数字货币量化投资课程》
-择时策略魔改研究
+择时策略魔改研究小组（第1期）
+无需编程基础，助教答疑服务，专属策略网站，一旦加入，永续更新。
+课程详细介绍：https://quantclass.cn/crypto/class
+邢不行微信: xbx9585
+本程序作者: 邢不行
+"""
+import pandas as pd
+import numpy as np
+import itertools
+
+
+# ======= 策略评价 =========
+# 将资金曲线数据，转化为交易数据
+def transfer_equity_c
