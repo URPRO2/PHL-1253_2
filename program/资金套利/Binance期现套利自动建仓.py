2260a4dafab4aacf323b2486c85c85a5c9e07447
URPRO2 <110991504+URPRO2@users.noreply.github.com>
2023-02-11 20:02:48 +0000

    Update algorithms.md

--- program/资金套利/Binance期现套利自动建仓.py
+++ program/资金套利/Binance期现套利自动建仓.py
@@ -6,4 +6,10 @@
 import ccxt
 from Function import *
 
-print(ccxt
+print(ccxt.__version__)  # 检查ccxt版本，需要最新版本，1.44.21以上
+
+# ===参数设定
+coin = 'btc'.upper()  # 要套利的币种
+future_date = '210625'  # 要套利的合约到期时间
+coin_precision = 1  # 去行情页面上，看下这个币种合约的价格是小数点后几位。如果小数点后有3位，那么coin_precision就是3
+execute_a
